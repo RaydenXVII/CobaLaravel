de6cc3ddc0bbb6dd92db7b7626dcb7602ce730e4
RaydenXVII <wantedgamer03@gmail.com>
2024-04-08 21:17:13 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,12 +10,12 @@ class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     *  // \App\Models\User::factory(10)->create();
      *
      * @return void
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
         Restaurant::factory(10)->create();
     }
 }
